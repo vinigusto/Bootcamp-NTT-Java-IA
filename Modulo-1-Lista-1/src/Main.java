71c2ba165383a3b61c784ddb961f7a7f1a5ee2ab
VINICIUS A SOUZA <vinigusto.souza@gmail.com>
2025-07-17 11:15:42 -0300

    Modulo-1-Lista-1
    Resolução dos Exercícios da Lista

---
+++ Modulo-1-Lista-1/src/Main.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in); // Scanner único
+
+    public static void main(String[] args) {
+        int opcao;
+        boolean continuar = true;
+
+        while(continuar){
+            do {
+                System.out.println("### Lista de Exercícios 1 ###\n" +
+                        "Escolha qual programa quer executar:\n" +
+                        "1 - Escreva nome e idade, retorna frase.\n" +
+                        "2 - Entra com o tamanho do lado de um quadrado, calcula e retorna area.\n" +
+                        "3 - Entre com base e altura de um triangulo, calcula e retorna area.\n" +
+                        "4 - Receba nome e idade de duas pessoas, calcula e retorna diferenca das idades.\n" +
+                        "0 - Sair."
+                );
+
+                System.out.print("Opcao Escolhida: ");
+                opcao = scanner.nextInt();
+                scanner.nextLine();
+
+                if(opcao < 0 || opcao > 4)
+                {
+                    System.out.println("Opcao Invalida. Tente Novamente.\n");
+                }
+
+            } while(opcao < 0 || opcao > 4);
+
+            switch(opcao){
+                case 1 -> Ex1();
+                case 2 -> Ex2();
+                case 3 -> Ex3();
+                case 4 -> Ex4();
+                case 0 -> continuar = false;
+            }
+        }
+
+        System.out.println("Programa Encerrado.");
+        scanner.close();
+    }
+
+    public static void Ex1()
+    {
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Olá " + nome + ", você tem " + idade + " anos.");
+    }
+
+    public static void Ex2()
+    {
+        System.out.print("Digite o tamanho do lado de um quadrado: ");
+        double lado = scanner.nextDouble();
+        scanner.nextLine();
+        double area = lado * lado;
+
+        System.out.println("O quadrado em questao tem uma area de tamanho: " + area);
+    }
+
+    public static void Ex3()
+    {
+        double altura, base, area;
+
+        System.out.print("Entre com a altura do triangulo: ");
+        altura = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("Entre com a base do triangulo: ");
+        base = scanner.nextDouble();
+        scanner.nextLine();
+
+        area = (base * altura)/2;
+
+        System.out.println("Um triangulo de base " + base +
+                " e altura " + altura +
+                " tem uma area total de: " + area);
+    }
+
+    public static void Ex4()
+    {
+        String nome1, nome2;
+        int idade1, idade2;
+
+        System.out.print("Entre com o nome da pessoa 1: ");
+        nome1 = scanner.nextLine();
+        System.out.print("Entre com a idade da pessoa 1: ");
+        idade1 = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Entre com o nome da pessoa 2: ");
+        nome2 = scanner.nextLine();
+        System.out.print("Entre com a idade da pessoa 2: ");
+        idade2 = scanner.nextInt();
+        scanner.nextLine();
+
+        if(idade1 > idade2) System.out.println(nome1 + " eh " + (idade1-idade2) + " mais velho(a) que " + nome2);
+        else if (idade2 > idade1) System.out.println(nome2 + " eh " + (idade2-idade1) + " mais velho(a) que " + nome1);
+        else System.out.println(nome1 + " tem a mesma idade que " + nome2);
+    }
+}
